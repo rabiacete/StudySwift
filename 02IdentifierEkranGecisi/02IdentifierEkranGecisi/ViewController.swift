07fdddb18833cc3b483e86d0f0c3456e13544247
rabiacete <rabiacete6@gmail.com>
2025-10-03 22:18:03 +0300

    02 Identifier ile ekran geçişi

---
+++ 02IdentifierEkranGecisi/02IdentifierEkranGecisi/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  02IdentifierEkranGecisi
+//
+//  Created by Rabia Çete on 3.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func btnSegueGec_TUI(_ sender: Any) {
+        
+        //segue başlatma
+        performSegue(withIdentifier: "sgGec", sender: nil)
+    }
+    
+    
+    @IBAction func btnGecIdentifier_TUI(_ sender: Any) {
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "VC2")
+        
+        present(vc, animated: true, completion: nil)// gösterme işlemi
+        
+        
+    }
+    
+}
+
